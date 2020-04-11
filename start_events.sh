73ef2df2b610097183adac7f37d9c9b5800d681c
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2020-04-11 17:59:15 +0200

    More logs

--- start_events.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-LOG_FILE=$SCRIPT_DIR/events_log.txt
-
-python tellstick_events.py > $LOG_FILE
--- start_gunicorn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-LOG_FILE=$SCRIPT_DIR/cronlog.txt
-
-cd $SCRIPT_DIR
-#sudo gunicorn -b 0.0.0.0:80 smarthus:app --daemon --pid 104711
-gunicorn smarthus:app
